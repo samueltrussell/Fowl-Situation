af5aa563f396887ad90be42cd278e9132e019b3a
Sam <samuel.t.russell@gmail.com>
2015-11-03 10:49:59 -0700

    Test project commit

---
+++ Fowl Situation - Test 1/Assets/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Rigidbody playerRigidbody;
+	public float speed;
+
+	private Vector3 targetPosition;
+	private Vector3 movement;
+
+
+	void Awake()
+	{
+		targetPosition = transform.position;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h, v);
+	}
+
+	void Move(float h, float v)
+	{
+		movement = (targetPosition - transform.position);
+		movement = movement.normalized * speed * Time.fixedDeltaTime;
+		playerRigidbody.MovePosition (transform.position + movement);
+
+	}
+
+	public void UpdateTargetPosition(Vector3 i_targetPosition)
+	{
+		targetPosition = i_targetPosition;
+	}
+
+}
