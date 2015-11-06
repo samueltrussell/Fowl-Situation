fda9d0225a5e17578d3c7c0119b4bbc0b307bf93
AjinkyDhote <ajinkya65@gmail.com>
2015-11-05 22:08:53 -0700

    Added new prefab for EnemyManger which is EnemyManager1 with improved spawning. Remove the old EnemyManager prefab and use this one.

--- Fowl Situation/Assets/Scripts/EnemyManager.cs
+++ Fowl Situation/Assets/Scripts/EnemyManager.cs
@@ -1,34 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class EnemyManager : MonoBehaviour {
-
-	public GameObject enemy;
-	public float spawnTime = 3f;
-	public Transform[] spawnPoints;
-	//public GameObject player;
-
-	// Use this for initialization
-	void Start () 
-	{
-		InvokeRepeating ("Spawn", spawnTime, spawnTime);
-		//player = GameObject.FindGameObjectWithTag("Player");
+	
+	public int totalEnemies;
+	public int numberOfEnemies;
 
 
-	}
 
-
-	void Spawn()
-	{
-
-
-		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
-
-		Instantiate (enemy, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
-		
-
-
-	}
+}
 	
 		
-}
+
