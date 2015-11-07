7be62caa9a3dbc07c3ead0abc632c9a20e4841a2
Sam <samuel.t.russell@gmail.com>
2015-11-07 15:45:12 -0700

    Adding files to support new animations, UI, and thePatsy

---
+++ Fowl Situation/Assets/Scripts/ObjectiveMessageManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectiveMessageManager : MonoBehaviour {
+
+	public float lingerTime = 2;
+	public bool win = false;
+	private float startTime;
+
+	// Use this for initialization
+	void Start () {
+		startTime = Time.timeSinceLevelLoad;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Time.timeSinceLevelLoad > (startTime + lingerTime)) {
+			if(win){
+
+			}else{
+				Destroy (this.gameObject);
+			}
+		}
+	}
+}
---
+++ Fowl Situation/Assets/Scripts/PlayerSoundManager.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSoundManager : MonoBehaviour {
+
+	public AudioClip punchWhiff;
+	public AudioClip punchCluckHit;
+	public AudioClip punchNoCluck;	
+	public AudioClip spinWhiff;
+	public AudioClip spinHit;
+	public AudioClip idleClucking;
+	public AudioClip deathRattle;
+	public AudioSource audioSource;
+
+
+
+	// Use this for initialization
+	void Start () {
+		//audioSource = GetComponent<AudioSource> ();
+	}
+
+	void idleSound()
+	{
+		int chance = Random.Range (0, 10);
+		if (chance == 1) {
+			playIdleClucking();
+		}
+
+	}
+
+	void playIdleClucking()
+	{
+		audioSource.clip = idleClucking;
+		audioSource.Play();
+	}
+
+	public void punchHitSound()
+	{
+		if (Random.Range (0, 3) == 1) {
+			audioSource.PlayOneShot (punchCluckHit);
+		} 
+		//else {
+			audioSource.PlayOneShot (punchNoCluck);
+		//}
+	}
+
+	public void spinHitSound()
+	{
+		audioSource.PlayOneShot (spinHit);
+	}
+
+	void playPunchWhiff()
+	{
+		audioSource.PlayOneShot (punchWhiff);
+	}
+
+	void playPunchCluckHit()
+	{
+		audioSource.PlayOneShot (punchCluckHit);
+	}
+
+	void playPunchNoCluck()
+	{
+		audioSource.PlayOneShot (punchNoCluck);
+	}
+
+	void playSpinWhiff()
+	{
+		audioSource.PlayOneShot (spinWhiff);
+	}
+
+	void playSpinHit()
+	{
+		audioSource.PlayOneShot (spinHit);
+	}
+
+	void playDeathRattle()
+	{
+		audioSource.PlayOneShot (deathRattle);
+	}
+}
