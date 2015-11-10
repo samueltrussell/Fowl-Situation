407588c7e085ad1519b1ff50a077d66d1db89602
AjinkyDhote <ajinkya65@gmail.com>
2015-11-10 15:31:59 -0700

    Forgot to add ScoreManager

---
+++ Fowl Situation/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public static int score;
+
+	Text text;
+
+	void Awake () 
+	{
+		text = GetComponent<Text> ();
+		score = 0;
+	}
+	
+
+	void Update () 
+	{
+		text.text = "Score : " + score; 
+	}
+
+}
